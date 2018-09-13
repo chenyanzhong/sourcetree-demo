9f046b93c52a80e3a502abd4d46bab26154d2bbc
chenyanzhong <chen_yanzhong@outlook.com>
2018-09-13 09:44:12 +0800

    增加配置文件

---
+++ pages/config.js
@@ -0,0 +1,20 @@
+// 配置文件
+const isLocal = false;
+const testServer = true;
+
+module.exports = {
+  testServer: testServer,
+  host: isLocal ? 'http://localhost:8080/' : testServer ? 'https://test.xmiles.cn/' : 'https://xmiles.cn/',
+
+  pversion: '53',
+  cversion: 53,
+  cversionname: '1.0',
+  channel: '1000',
+  prdid: '17000',
+
+  shareContent: {
+    title: '我正在用微信步数兑换以下商品，助我一臂之力',
+    imageUrl: 'https://img.xmiles.cn/wechat/shareicon.png',
+    path: '/pages/index/index'
+  }
+}
